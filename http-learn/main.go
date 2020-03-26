e9e7863b76b966662e13c66d155368397d267cfc
jsjchai <jsjchai@163.com>
2020-03-26 10:16:16 +0800

    http helloworld

---
+++ http-learn/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type httpHandler struct {
+}
+
+func (h *httpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("request url:" + r.Host + "" + r.RequestURI)
+	_, _ = w.Write([]byte("hello,world"))
+}
+
+func main() {
+	http.Handle("/hello", &httpHandler{})
+	_ = http.ListenAndServe(":9898", nil)
+
+}
